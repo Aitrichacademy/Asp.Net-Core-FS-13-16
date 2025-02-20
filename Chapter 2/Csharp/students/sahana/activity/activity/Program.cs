315aad9bd91d9f089019ddbb3801e5308756b7ae
sahanaksks194@gmail.com <sahanaksks194@gmail.com>
2025-02-20 10:47:08 +0530

    add

---
+++ Chapter 2/Csharp/students/sahana/activity/activity/Program.cs
@@ -0,0 +1,259 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace activity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //swap
+            //Console.WriteLine("enter the number");
+            //int num1=Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("enter the number");
+            //int num2=Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"before swapping num1={num1},num2={num2}");
+
+            //int temp = num1;
+            //num1= num2;
+            //num2=temp;
+
+            //Console.WriteLine($"after swapping num1={num1},num={num2}");
+
+
+            //Console.WriteLine("enter the number");
+            //int num1=Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"multiplication:{num1}");
+            ////int num2=Convert.ToInt32(Console.ReadLine());
+
+
+            //for (int i = 1; i <=10; i++)
+            //{
+
+
+            //    Console.WriteLine($"the numner{num1}*{i}={num1 *i}");
+
+            //}
+
+            //Console.WriteLine("enter the number");
+            //int num1=Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("enter the number");
+            //int num2=Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("enter the number");
+            //int num3=Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("enter the number");
+            //int num4=Convert.ToInt32(Console.ReadLine());
+
+            //   int sum = 0;
+
+            //for (int i = 0; i < num1; i++)
+            //   {
+            //       sum = num1 + num2;
+            //       int avg=sum / num2;
+            //       Console.WriteLine(avg);
+
+            //   }
+
+            //Ai intelligents
+
+
+            //Console.WriteLine("enter the passcode");
+            //string s1 = Console.ReadLine();
+
+            //if (s1 == "true")
+            //{
+            //    Console.WriteLine("welcome");
+            //    Console.WriteLine("enter the passcode");
+            //    int n = Convert.ToInt32(Console.ReadLine());
+
+            //    if (n <= 5)
+            //    {
+
+            //        Console.WriteLine("allowed");
+            //        n = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("not allowed");
+            //    }
+            //}
+
+
+            //    Console.WriteLine("enter passcord");
+            //    string s2= Console.ReadLine();
+            //if (s2 == "no")
+            //{
+            //    Console.WriteLine("enter pass");
+            //    int n2 = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("retry");
+            //    }
+            //else
+            //{
+            //    Console.WriteLine("welcome");
+            //}
+
+
+
+            //Console.WriteLine("welcome to rescue team");
+            //string rescue=Console.ReadLine();
+
+            //if (rescue == "false")
+            //{
+            //    //Console.WriteLine($"rescue needed{false}");
+            //    Console.WriteLine(rescue="false");
+            //    Console.WriteLine("needed");
+            //    //string s=Console.ReadLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("rescue needed");
+            //}
+            //if(rescue == "true")
+            //{
+            //    Console.WriteLine($"rescue not needed:{true}");
+            //    //string s2=Console.ReadLine();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("planet will reached");
+            //}
+
+
+            //ai mood
+
+            //Console.WriteLine("enter feelings");
+            //string feeling = Console.ReadLine();
+
+            //if (feeling == "happy")
+            //{
+            //    Console.WriteLine("go out and celebrates");
+            //}
+            //else if (feeling == "sad")
+            //{
+            //    Console.WriteLine("watch comfort movie");
+            //}
+            //else if (feeling == "angry")
+            //{
+            //    Console.WriteLine("meditation");
+            //}
+            //else if (feeling == "bored")
+            //{
+            //    Console.WriteLine("read a book or try new hobby");
+            //}
+            //else if (feeling == "exited")
+            //{
+            //    Console.WriteLine("shared good news with your freiends");
+            //}
+            //else if (feeling == "tired")
+            //{
+            //    Console.WriteLine("take a nap or rest");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("mood not regagaised.try expressing yourself more clearly");
+            //}
+
+            //Console.WriteLine("enter your code");
+            //int n=Convert.ToInt32(Console.ReadLine());
+
+            //if (n == 1.50)
+            //{
+            //    Console.WriteLine("coke");
+            //}
+            //else if (n == 1.00)
+            //{
+            //    Console.WriteLine("chips");
+            //}
+
+            //else if (n == 2.00)
+            //{
+            //    Console.WriteLine("chocalate");
+            //}
+            //else if (n == 1.00)
+            //{
+            //    Console.WriteLine("water");
+            //}
+            //else if (n == 1.75)
+            //{
+            //    Console.WriteLine("juice");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("invalid selection.please try again");
+            //}
+
+
+            //Console.WriteLine("enter the code (1-5)");
+            //int productcode=Convert.ToInt32(Console.ReadLine());
+
+            //switch(productcode)
+            //{
+            //    case 1:
+            //        Console.WriteLine("coke $1.50");
+            //        break;
+            //        case 2:
+            //        Console.WriteLine("chips $1.00");
+            //        break;
+            //        case 3:
+            //        Console.WriteLine("chocolate $2.00");
+            //        break;
+            //        case 4:
+            //        Console.WriteLine("water $1.00");
+            //        break;
+            //        case 5:
+            //        Console.WriteLine("juice $1.75");
+            //        break;
+            //    default:
+            //        Console.WriteLine("inavlide:please try again");
+            //        break;
+
+            //}
+
+
+            //Console.WriteLine("enter the command code");
+            //string pannel=Console.ReadLine();
+
+            //switch(pannel)
+            //{
+            //    case "lunch":
+            //        Console.WriteLine(" initaling lunch sequence");
+            //        break;
+            //    case "abort":
+            //        Console.WriteLine("mission aborted ! returning to stand by mode");
+            //        break;
+            //    case "status":
+            //        Console.WriteLine("all system operational");
+            //        break;
+            //    case "self-distruct":
+            //        Console.WriteLine("warning self requested intiatilized needed");
+            //        break;
+            //    case "reboot":
+            //        Console.WriteLine("rebooting all system ....please wait");
+            //        break;
+            //        default :
+            //        Console.WriteLine("invalid command.please enter valid operations!");
+            //        break;
+            //}
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+    }
+          
+
+           
+        
---
+++ Chapter 2/Csharp/students/sahana/activity/structure/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace structure
+{
+    internal class Program
+    {
+        public struct student
+        {
+            public string name;
+            public int age;
+            public int mark;
+
+            public student(string name, int age, int mark)
+            {
+                this.name = name;
+                this.age = age;
+                this.mark = mark;
+            }
+            public void display()
+            {
+                Console.WriteLine($"Name:{name},age:{age},mark:{mark};");
+            }
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            student student1;
+            student1.name = "sahana";
+            student1.age = 23;
+            student1.mark = 90;
+            student1.display();
+            //structure  multiple namelek loop cheyyum
+
+            student student2 = new student("sahana", 30, 40);
+            student2.display();
+
+
+
+            //question
+
+        }
+    }
+    }
+
