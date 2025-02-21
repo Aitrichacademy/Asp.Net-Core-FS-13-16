5d4c5170ea2d555ab9e4d0c8afa64d5cb0155c27
arjunc812 <helloarjun@gmail.com>
2025-02-21 13:08:35 +0530

    added'

---
+++ Chapter 2/Csharp/Machine test/Arjun/Machine-Test-C#/Machine-Test-C#/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+struct Employee
+{
+    public int EmployeeID;
+    public string Name;
+    public double Salary;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee[] employees = new Employee[5];
+
+        
+        for (int i = 0; i < employees.Length; i++)
+        {
+            Console.WriteLine($"Enter the details of Employee {i + 1}:");
+
+            Console.Write("Enter EmployeeID: ");
+            employees[i].EmployeeID = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Name: ");
+            employees[i].Name = Console.ReadLine();
+
+            Console.Write("Enter Salary: ");
+            employees[i].Salary = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+        }
+
+        
+        double highest = employees[0].Salary;
+        double lowest = employees[0].Salary;
+
+        
+        foreach (Employee employee in employees)
+        {
+            if (employee.Salary > highest)
+                highest = employee.Salary;
+
+            if (employee.Salary < lowest)
+                lowest = employee.Salary;
+        }
+
+        
+        Console.WriteLine("Details of the Employees:");
+        for (int i = 0; i < employees.Length; i++)
+        {
+            Console.WriteLine($"EmployeeID: {employees[i].EmployeeID}, Name: {employees[i].Name}, Salary: {employees[i].Salary}");
+        }
+
+        Console.WriteLine($"Highest Salary: {highest}");
+        Console.WriteLine($"Lowest Salary: {lowest}");
+    }
+}
